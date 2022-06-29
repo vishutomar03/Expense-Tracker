05e0b2d5ab9547a68d90cfcd294870e091d52d6c
Shivani Tomar <vishu03tomar@gmail.com>
2022-06-29 15:38:02 +0530

    first commit

---
+++ src/components/InfoCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const isIncome = Math.round(Math.random());
+
+const InfoCard = (props) => {
+  // if (props.save < 0) {
+  //   return (
+  //     <div elevation={3} style={{ textAlign: "center", padding: "0 10%" }}>
+  //       <span style={{ color: "red", "font-weight": "bold" }}>
+  //         Out Of MONEY !!!!.
+  //       </span>
+  //     </div>
+  //   );
+  // }
+  // if (props.save < 100) {
+  //   return (
+  //     <div elevation={3} style={{ textAlign: "center", padding: "0 10%" }}>
+  //       <span style={{ color: "yellow", "font-weight": "bold" }}>Low.</span>
+  //     </div>
+  //   );
+  // }
+  return <div></div>;
+};
+
+export default InfoCard;
